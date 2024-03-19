e58115dabebcde1240cc48e7f3790ec206492287
Wyatt Schiefelbein <wdschiefe@gmail.com>
2024-03-19 16:11:51 -0600

    feature: create scramble_exam function in randomizer.py
    to integrate randomizer.py into scrambler

--- randomizer.py
+++ randomizer.py
@@ -2,6 +2,9 @@ import csv
 import random
 import argparse
 
+from file_n_dirs import mk_dir
+from file_n_dirs import rename_file
+from pathlib import Path
 
 '''Originally developed by dnswrsrx. Incorporated March 19, 2024, from 
 https://gitlab.com/dnswrsrx/shenanigans/-/blob/master/wyatts_assessment_randomizer/randomizer.py?ref_type=heads
@@ -43,15 +46,15 @@ class Question:
         answer_file_fp.write(f'{index}. {as_alpha(self.options.index(self.correct))}\n')
 
 
-def generate_tests(csv_file_name, shuffle_options, number_of_tests):
+def generate_tests(csv_file_name, name, shuffle_options, number_of_tests):
 
     questions = collect_questions(csv_file_name)
 
     for index in range(number_of_tests):
         # Add number into file name if more than one tests to be created
         index_addition = f'_{index+1}' if number_of_tests > 1 else ''
-        test_file_name = f'test{index_addition}.txt'
-        answer_file_name = f'answer{index_addition}.txt'
+        test_file_name = f'{name}{index_addition}.txt'
+        answer_file_name = f'{name}{index_addition}, Answer Key.txt'
 
         # Opening multiple files within the same with block
         # so that as I go through each question,
@@ -67,7 +70,7 @@ def generate_tests(csv_file_name, shuffle_options, number_of_tests):
                 question.append_to_test_file(test_file, question_index, shuffle_options)
                 question.append_to_answer_file(answer_file, question_index)
 
-
+"""
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -85,3 +88,30 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     generate_tests(args.csv_file, args.shuffle, args.number)
+"""
+
+def scramble_exam(exam_name: str, exam_bank_file: str, num_ver: int, yn_shuffle_qs: str):
+    '''Calls generate_tests function and prints exam and answer keys to documents folder.
+    Prints a user friendly message to terminal and informs user of where the exams and
+    keyts can be found'''
+
+
+    home_path = Path.home()     # /Users/[name]/
+    print_to_path = home_path / 'Documents'
+    exam_dir = mk_dir(print_to_path / exam_name)
+
+    # Text to print to terminal
+    separator = '\n*********************************\n'
+    intro = "tân'si! I will scramble your exam now.\n"
+    print(separator)
+    print(intro)
+
+    generate_tests(exam_bank_file,exam_name,yn_shuffle_qs,num_ver)
+
+    #  print to terminal
+    outro = '''Aaaaand, DONE!
+    \nThe exam(s) and the answer key(s) can be found at\t{}.\n\nêkosi mâka!'''
+    print(outro.format(exam_dir))
+    print(separator)
+
+scramble_exam("Test 1", "")
