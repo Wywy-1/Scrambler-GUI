5b5d541173d42a0f8d00e7ca51db6f567875d3e2
Wyatt Schiefelbein <schiefel@ualberta.ca>
2024-03-28 00:19:18 -0600

    refactor: change date/time format

--- dates.py
+++ dates.py
@@ -3,6 +3,5 @@ import datetime as dt
 def get_time():
     """Returns the current date and time as 'MMM-dd,HH:MM:SS."""
     date = dt.datetime.now()
-    print("Date is\t\t{}".format(date.strftime("%h-%d,%Y, %H:%M:%S")))
 
-    return str(date.strftime("%h-%d,%Y,%H:%M:%S"))
+    return str(date.strftime("%h %d,%Y, at %H-%M-%S"))
